a6c4354e04480b301ac62728b1e468dfb2b7a3a9
aasifshahzad <71362700+aasifshahzad@users.noreply.github.com>
2023-03-26 07:33:18 +0500

    step00(a,ab,c,d),step01 and step02

---
+++ step00/app.ts
@@ -0,0 +1,66 @@
+let fname = 'Asif';
+console.log(`Hello ${fname} Iam at step 0 of TS learning.`);
+
+//Json object
+ // JSON object
+
+ const detail = { "name": "Vipin", "age": 21 }
+
+ // accessing JSON object
+
+ console.log(detail. name); // Vipin
+ 
+ console.log(detail. age); // Vipin
+
+ // Syntax error
+//  lett message = "Hello World";//syntax error
+// console.log(message);
+
+//Type error
+//let message = "Hello World";
+//console.loger(message);
+
+//Assignabality error
+let message = "Hello World";
+//message = 6;//Type 'number' is not assignable to type 'string'.
+console.log(message);
+
+//STEP 01
+//strongly typed syntax
+let a : string = "Pakistan";
+a = "USA";
+let b : number = 9;
+let c : boolean = true;
+
+//type inference
+let e = "USA";
+let f = 10.9;
+f = 22;
+let g = false;
+
+
+//STEP 02
+
+//use const where variable values do not change
+const h = 5;
+const i : number = 33;
+const j ="best";
+
+//I suggest use let instead of var everywhere, 
+//becuase let has blocked scope
+if (g == false) {
+	var zNumber = 4;
+    console.log(zNumber);
+    
+	//use z
+}
+else {
+	var zString = "string";
+    console.log(zString);
+    
+	//use z
+}
+//console.log("let: " + zNumber);// Error: z is not defined in this scope ReferenceError: z is not defined
+
+
+        
